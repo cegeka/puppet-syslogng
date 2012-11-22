12657bcef6e5d85d8b17e5f240ecbbb1f7b41b6f
fdammeke <fabian.dammekens@cegeka.be>
2012-11-22 17:55:33 +0100

    Initial syslogng module

--- spec/classes/syslogng_spec.rb
+++ spec/classes/syslogng_spec.rb
@@ -3,5 +3,7 @@
 require 'spec_helper'
 
 describe 'syslogng' do
+  let (:facts) {{ :operatingsystem => 'redhat' }}
+  let (:params) {{ :ensure => 'present', :loghost => 'dummyhost', :logdir => '/var/log' }}
   it { should contain_class 'syslogng' }
 end
