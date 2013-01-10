db3dd2d211f527a68ba236df2ff778e8c14a1a28
Kristof Willaert <kristof.willaert@cegeka.be>
2013-01-10 10:04:16 +0100

    Add defined type for log sources

--- manifests/config/source.pp
+++ manifests/config/source.pp
@@ -1,3 +1,40 @@
-define syslogng::config::source() {
+define syslogng::config::source (
+                                  $ensure = 'present',
+                                  $type   = 'file',
+                                  $source = undef,
+                                  $options = []
+                                )
+{
+  case $ensure {
+    'absent': {
+      include syslogng::params
 
+      file { "source_${title}":
+        ensure => $ensure,
+        path   => "${syslogng::params::conf_dir}/includes/source/${title}.inc"
+      }
+    }
+    'present': {
+      if $source {
+        include syslogng::params
+
+        file { "source_${title}":
+          ensure  => 'file',
+          owner   => $syslogng::params::user,
+          group   => $syslogng::params::group,
+          mode    => '0644',
+          path    => "${syslogng::params::conf_dir}/includes/source/${title}.inc",
+          notify  => Class['syslogng::service'],
+          content => template('syslogng/config/source.erb')
+        }
+
+      }
+      else {
+        fail("Syslogng::Config::Source['${title}']: required parameter source not specified")
+      }
+    }
+    default: {
+      fail("Syslogng::Config::Source['${title}']: parameter ensure must be present or absent")
+    }
+  }
 }
