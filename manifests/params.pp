a93665daab6b670b0cb0ba3f56dafba515eaa865
Radu Catalin Andrei <radu-catalin.andrei@cegeka.com>
2024-09-25 13:29:31 +0300

    [Syslog-ng] Add rhel9 support

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,16 +4,18 @@ class syslogng::params {
     'RedHat': {
       $user              = 'root'
       $group             = 'root'
-      $package           = ['syslog-ng', 'syslog-ng-libdbi']
       $service           = 'syslog-ng'
       $service_hasstatus = true
       $conf_dir          = '/etc/syslog-ng'
       $conf_file         = 'syslog-ng.conf'
       $sysconf_dir       = '/etc/sysconfig'
       case $facts['os']['release']['major'] {
-        '6': { $config_template = 'syslog-ng.conf-el6.erb' }
-        '7': { $config_template = 'syslog-ng.conf-el7.erb' }
-        '8': { $config_template = 'syslog-ng.conf-el8.erb' }
+        '7': { $config_template = 'syslog-ng.conf-el7.erb'
+               $package         = ['syslog-ng', 'syslog-ng-libdbi'] }
+        '8': { $config_template = 'syslog-ng.conf-el8.erb'
+               $package         = ['syslog-ng', 'syslog-ng-libdbi'] }
+        '9': { $config_template = 'syslog-ng.conf-el9.erb'
+               $package         = 'syslog-ng'}
         default: {}
       }
     }
